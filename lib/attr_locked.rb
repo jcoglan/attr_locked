e529f2496a55854943fda258c5dba33e12951627
James Coglan <jcoglan@googlemail.com>
2007-06-08 12:10:39 +0000

    Added methods that intercept AR::Base.attr= and AR::Base.update_attribute. Attributes cannot be modified unless the record is new, or unless you access @attributes directly. Added attr_locked macro.

--- lib/attr_locked.rb
+++ lib/attr_locked.rb
@@ -1,1 +1,32 @@
-# AttrLocked
+module ActiveRecord #:nodoc:
+  class Base #:nodoc:
+    class << self
+      def attr_locked(*attributes)
+        write_inheritable_array("attr_locked", attributes - (locked_attributes || []))
+        define_locked_write_methods
+      end
+      
+      def locked_attributes
+        read_inheritable_attribute("attr_locked")
+      end
+      
+      def define_locked_write_methods
+        locked_attributes.to_a.collect(&:to_s).each do |attr|
+          define_method("#{attr}=") do |value|
+            @attributes[attr] = value if new_record?
+          end
+        end
+      end
+    end
+    
+    def update_attribute_with_attribute_locking(name, value)
+      if self.class.locked_attributes.to_a.collect(&:to_s).include?(name.to_s)
+        return false
+      else
+        update_attribute_without_attribute_locking(name, value)
+      end
+    end
+    alias_method(:update_attribute_without_attribute_locking, :update_attribute)
+    alias_method(:update_attribute, :update_attribute_with_attribute_locking)
+  end
+end
